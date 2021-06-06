ad3f764858c07a1232e40046687f92dd7432fc27
naresh <nareshtamang20000@gmail.com>
2021-06-06 19:44:42 +0545

    new

--- js/script.js
+++ js/script.js
@@ -52,15 +52,23 @@ document.write("<br>");
 document.write(strong(12));
 
 
-var a=[12, 35, 45, 65, 7598, 2250, 100000, 2500 ];
-var max=a[0];
-for(i=0, i<6,i++ ) {
-    if(max>a[i]){
-        max=a[i];
-    }
-    document.write("the max num is"+max);
+var myPricelist = [4545 , 3434 , 23 , 34 ,45 , 324 , 5725, 727, 817 , 82 , 2 , 1 ]
+var max = myPricelist [0];
+var min = myPricelist [0];
+
+for( var i = 1; i < myPricelist.length; i++)
+{
+      if(max < myPricelist[i]){
+            max = myPricelist[i];
+      }
+      if(min > myPricelist[i]){
+            min = myPricelist[i];
+      }
 }
 
-
-
+document.write("Max = " + max);
+document.write("</br>");
+document.write("Min = " + min);
+//write a function to check given number is prime or not 
+//display the prime number
 
